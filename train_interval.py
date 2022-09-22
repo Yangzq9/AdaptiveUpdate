e280d95aac663c270b964101b8bbb109f279728c
Yangzq9 <113275029+Yangzq9@users.noreply.github.com>
2022-09-22 21:11:31 +0800

    Update train_interval.py

--- train_interval.py
+++ train_interval.py
@@ -21,10 +21,6 @@ with open('seed.txt', 'r+') as f:
     f.truncate()
     f.write(str(seed + 1))
 
-env_config = [['InvertedPendulumBulletEnv-v0', 5000, 500], ['InvertedDoublePendulumBulletEnv-v0', 25000, 500],
-              ['InvertedPendulumSwingupBulletEnv-v0', 3500, 500], ['HopperBulletEnv-v0', 25000, 500]]
-al_config = [['', 1.0, 1.0, 1, 'adaptive1'], ['', 1.0, 1.0, 3, 'adaptive1'], ['', 1.0, 1.0, 5, 'adaptive1']]
-
 def parse_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for adaptive update method of target network")
     # Environment
@@ -49,6 +45,10 @@ def parse_args():
     parser.add_argument("--display", action="store_true", default=False)
     parser.add_argument("--plots-dir", type=str, default="./Saveofsimple/learning_curves/", help="directory where plot data is saved")
     return parser.parse_args()
+
+env_config = [['InvertedPendulumBulletEnv-v0', 5000, 500], ['InvertedDoublePendulumBulletEnv-v0', 25000, 500],
+              ['InvertedPendulumSwingupBulletEnv-v0', 3500, 500], ['HopperBulletEnv-v0', 25000, 500]]
+al_config = [['', 1.0, 1.0, 1, 'adaptive1'], ['', 1.0, 1.0, 3, 'adaptive1'], ['', 1.0, 1.0, 5, 'adaptive1']]
 
 def mlp_model(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=None):
     # This model takes as input an observation and returns values of all actions
